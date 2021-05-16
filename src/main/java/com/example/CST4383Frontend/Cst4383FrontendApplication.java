f5f65a8b4ca535ff6089904afa7460d198ca2d96
Michael <Michaelecisneros@gmail.com>
2021-05-16 22:01:22 +0530

    Initial commit

---
+++ src/main/java/com/example/CST4383Frontend/Cst4383FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CST4383Frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4383FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst4383FrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CST4383Frontend/Cst4383FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CST4383Frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
